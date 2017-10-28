ddb1f24fed82b473cd29f93d2bb068ab160e83ac
vaenow <luowenbiz@gmail.com>
2017-10-28 21:35:43 +0800

    Translate long words

--- js/app.js
+++ js/app.js
@@ -12,15 +12,37 @@
 const tjs = require('translation.js')
 window.tjs2 = tjs
 
+const MAX_T_LEN = 4000
+
 const intervalNum = setInterval(() => {
 	console.log('detecting <video>...')
 	const captionInfo = getCaptionInfo()
-	if (captionInfo.content) {
+	if (captionInfo) {
 		clearInterval(intervalNum)
-		go.call(this, captionInfo.content)
-			.then(resp => insertCaption(resp.result, captionInfo))
+		goTranslateAndInsert(captionInfo)
 	}
 }, 500)
+
+function goTranslateAndInsert(captionInfo, captionIndexRange = [0, 0]) {
+	console.log('goTranslateAndInsert', captionIndexRange)
+
+	let content = ''
+	for (let i = captionIndexRange[0]; i < captionInfo.captions.length; i++) {
+		const caption = captionInfo.captions[i];
+		if ((content + caption).length > MAX_T_LEN) break;
+		content += caption
+		captionIndexRange[1]++
+	}
+	
+	go.call(this, content)
+		.then(resp => insertCaption(resp.result, captionInfo, captionIndexRange))
+		.then(() => {
+			const captionIndexEnd = captionIndexRange[1]
+			if (captionIndexEnd < captionInfo.captions.length) {
+				goTranslateAndInsert(captionInfo, [captionIndexEnd ,captionIndexEnd])
+			}
+		})
+}
 
 function getCaptionInfo() {
 	let content = "no caption"
@@ -32,13 +54,15 @@ function getCaptionInfo() {
 		var captions = Array.from(tt.cues).map((c, idx) =>
 			(idx !==0 ? `%%${idx}&&` : '') + c.text
 		)
+
+		if(!captions.length) return false
 		// content = captions.join(SEPARATOR)
-		content = captions.join('')
-		console.log('content', captions, content)
+		// content = captions.join('')
+		// console.log('content', captions, content)
 	} catch (e) {
 		return false
 	}
-	return { content, vtts, tt, SEPARATOR }
+	return { captions, content, vtts, tt, SEPARATOR }
 }
 
 function go(content) {
@@ -50,14 +74,17 @@ function go(content) {
 		})
 }
 
-function insertCaption(result, {vtts, SEPARATOR, tt}) {
+function insertCaption(result, {vtts, SEPARATOR, tt}, captionIndexRange) {
 	const content = result.join('')
 	const captions = content.split("%%")
 	console.log('captions', captions)
-	Array.from(tt.cues).forEach((c, idx) => {
-		c.text += `\n${findProperCaption(idx, captions)}`
-		console.log('c.text', c.text)
-	})
+
+	const cues = Array.from(tt.cues)
+
+	for (let i = captionIndexRange[0]; i<captionIndexRange[1]; i++) {
+		console.log('cues[i]', i)
+		cues[i].text += `\n${findProperCaption(i, captions)}`
+	}
 }
 
 function findProperCaption(index, captions) {
