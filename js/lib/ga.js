0931d3e5e26a2c41d87b189306cde13cd8da5471
vaenow <luowenbiz@gmail.com>
2017-11-02 23:19:47 +0800

    update

--- js/lib/ga.js
+++ js/lib/ga.js
@@ -8,7 +8,7 @@ window.dataLayer = window.dataLayer || [];
 function gtag(){dataLayer.push(arguments);}
 gtag('js', new Date());
 gtag('config', 'UA-106828486-2');
-ga('set', {'userId': 'ARGS'});
+ga('set', 'userId', 'ARGS');
 `;
 export default function (forceReset) {
     if (forceReset) $('script[name=crx]').remove();
