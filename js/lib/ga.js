a2fc64212068d92c9e9b027411071da02686ee99
vaenow <luowenbiz@gmail.com>
2017-11-03 09:38:31 +0800

    update tickˆ

--- js/lib/ga.js
+++ js/lib/ga.js
@@ -11,7 +11,8 @@ m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
 ga('create', 'UA-106828486-2', 'auto');
 `;
 const PAGE_VIEW = `ga('send', 'pageview');`;
-const TICK_EVENT = `ga('send', 'event', 'ARGS', 'tick');`;
+//                  ga('send', 'event', 'Videos', 'play', 'Fall Campaign');
+const TICK_EVENT = `ga('send', 'event', 'Tick', 'tick', 'ARGS');`;
 // `
 // window.dataLayer = window.dataLayer || [];
 // function gtag(){dataLayer.push(arguments);}
